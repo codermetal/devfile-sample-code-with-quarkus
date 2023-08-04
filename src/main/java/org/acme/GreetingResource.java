c16c17e94a4d24b1c427064327ac1003c9d73b95
ciolak74 <42117544+ciolak74@users.noreply.github.com>
2023-08-04 13:09:03 +0200

    4

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -11,6 +11,6 @@ public class GreetingResource {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello3 RESTEasy";
+        return "Hello4 RESTEasy";
     }
 }
